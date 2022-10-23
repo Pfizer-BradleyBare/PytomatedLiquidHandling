9f07357eea577ad5249311e1a40b22e9bb49031e
Bare <BAREB@pfizer.com>
2022-10-23 08:24:09 -0500

    Final update of Tracker... I hope

--- ABN/Source/AbstractClasses/Tracker/TrackerABC.py
+++ ABN/Source/AbstractClasses/Tracker/TrackerABC.py
@@ -1,6 +1,9 @@
 from abc import abstractmethod
 from ..Object.ObjectABC import ObjectABC
 
+# NOTE: This is an abstract base class but for all intents and purposes the methods can be directly copied and used. However,
+# do not forget to change the type information!
+
 
 class TrackerABC:
     @abstractmethod
@@ -8,16 +11,23 @@ class TrackerABC:
         self.Collection: dict[str, ObjectABC] = dict()
 
     @abstractmethod
-    def ManualLoad(self, ObjectABCInstance: ObjectABC):
-        raise NotImplementedError
+    def ManualLoad(self, ObjectABCInstance: ObjectABC) -> None:
+
+        if self.IsTracked(ObjectABCInstance) is True:
+            raise (str(type(ObjectABCInstance).__name__)) + " is already tracked"
+
+        self.Collection[ObjectABCInstance.GetName()] = ObjectABCInstance
 
     @abstractmethod
-    def ManualUnload(self, ObjectABCInstance: ObjectABC):
-        raise NotImplementedError
+    def ManualUnload(self, ObjectABCInstance: ObjectABC) -> None:
+        if self.IsTracked(ObjectABCInstance) is False:
+            raise (str(type(ObjectABCInstance).__name__)) + " is not yet tracked"
+
+        del self.Collection[ObjectABCInstance.GetName()]
 
     @abstractmethod
-    def IsTracked(self, ObjectABCInstance: ObjectABC):
-        raise NotImplementedError
+    def IsTracked(self, ObjectABCInstance: ObjectABC) -> bool:
+        return ObjectABCInstance.GetName() in self.Collection
 
     @abstractmethod
     def GetObjectsAsList(self) -> list[ObjectABC]:
