45f2211870cb13503c0c64ded12223246a6814bd
Bradley Bare <BRadley.Bare@pfizer.com>
2022-11-02 22:03:22 -0500

    Error reporting.

--- ABN/Source/API/Workbook/Solution/Value/ValueTracker.py
+++ ABN/Source/API/Workbook/Solution/Value/ValueTracker.py
@@ -5,6 +5,18 @@ from .Value import SolutionPropertyValue
 class SolutionPropertyValueTracker(TrackerABC[SolutionPropertyValue]):
     def GetObjectByNumericValue(self, NumericValue: int) -> SolutionPropertyValue:  # type: ignore
 
+        ReturnSolution: SolutionPropertyValue | None
+        ReturnSolution = None
+
         for Object in self.GetObjectsAsList():
             if Object.GetNumericValue() == NumericValue:
-                return Object
+                ReturnSolution = Object
+
+        if ReturnSolution is None:
+            raise Exception(
+                "SolutionPropertyValueTracker - "
+                + str(NumericValue)
+                + " is not tracked."
+            )
+
+        return ReturnSolution
