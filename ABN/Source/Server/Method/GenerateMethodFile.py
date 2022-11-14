04565e5bdbcc1bc88308ff4af9d8535fec647a61
Bradley Bare <BRadley.Bare@pfizer.com>
2022-11-14 08:47:11 -0600

    Remove debugging prints

--- ABN/Source/Server/Method/GenerateMethodFile.py
+++ ABN/Source/Server/Method/GenerateMethodFile.py
@@ -58,11 +58,8 @@ class GenerateMethodFile:
         os.chmod(DesiredMethodFilePath, stat.S_IWRITE)
 
         ExcelInstance = Excel(DesiredMethodFilePath)
-        print("Pre-attach")
         with ExcelHandle(False) as ExcelHandleInstance:
-            print("Pre-attach")
             ExcelInstance.AttachHandle(ExcelHandleInstance)
-            print("Post")
 
             ExcelInstance.SelectSheet("Worklist")
             CopyFormula = cast(
