98ea6e7880d15c7c36b759222ada1d4564d965d5
Bradley Bare <BRadley.Bare@pfizer.com>
2022-11-16 17:27:32 -0600

    Still working on loaded labware connection

--- ABN/Source/Driver/LoadedLabwareConnection/LoadedLabwareConnection.py
+++ ABN/Source/Driver/LoadedLabwareConnection/LoadedLabwareConnection.py
@@ -1,14 +1,16 @@
 from ...Tools.AbstractClasses import ObjectABC
 from ...API.Tools.Container import Container
 from ...HAL.Tools import LoadedLabware
+from ...HAL.Tools import LabwareSelection
 
 
 class LoadedLabwareConnection(ObjectABC):
     def __init__(
-        self, ContainerInstance: Container, LoadedLabwareInstance: LoadedLabware
+        self, ContainerInstance: Container, LabwareSelectionInstance: LabwareSelection
     ):
         self.ContainerInstance: Container = ContainerInstance
-        self.LoadedLabwareInstance: LoadedLabware = LoadedLabwareInstance
+        self.LabwareSelectionInstance: LabwareSelection = LabwareSelectionInstance
+        self.LoadedLabwareInstance: LoadedLabware | None = None
 
     def GetName(self) -> str:
         return self.ContainerInstance.GetName()
@@ -16,5 +18,16 @@ class LoadedLabwareConnection(ObjectABC):
     def GetContainerInstance(self) -> Container:
         return self.ContainerInstance
 
+    def IsConnected(self):
+        return self.LoadedLabwareInstance is not None
+
+    def GetLabwareSelection(self):
+        return self.LabwareSelectionInstance
+
     def GetLoadedLabwareInstance(self) -> LoadedLabware:
+        if self.LoadedLabwareInstance is None:
+            raise Exception(
+                "Loaded labware is none. Did you check if it is connected first?"
+            )
+
         return self.LoadedLabwareInstance
--- ABN/Source/HAL/Tools/__init__.py
+++ ABN/Source/HAL/Tools/__init__.py
@@ -1,5 +1,15 @@
 from .HalLock.HalLockTracker import HalLockTracker
 from .LoadedLabware.LoadedLabware import LoadedLabware
 from .LoadedLabware.LoadedLabwareTracker import LoadedLabwareTracker
+from .LabwareSelection.LabwareSelection import LabwareSelection
+from .LabwareSelection.LabwareSelectionTracker import LabwareSelectionTracker
+from .LabwareSelection.LabwareSelectionLoader import Load as LabwareSelectionLoader
 
-__all__ = ["HalLockTracker", "LoadedLabware", "LoadedLabwareTracker"]
+__all__ = [
+    "HalLockTracker",
+    "LoadedLabware",
+    "LoadedLabwareTracker",
+    "LabwareSelection",
+    "LabwareSelectionTracker",
+    "LabwareSelectionLoader",
+]
