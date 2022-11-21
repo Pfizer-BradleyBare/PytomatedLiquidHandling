870ace39a2bf10f5537e2664e9c7e8f93fdb6212
Bradley Bare <Bradley.Bare@pfizer.com>
2022-11-21 08:26:01 -0600

    Updated error message

--- ABN/Source/Driver/Tools/Command/CommandTracker.py
+++ ABN/Source/Driver/Tools/Command/CommandTracker.py
@@ -6,7 +6,7 @@ class CommandTracker(TrackerABC[Command]):
     def ManualLoad(self, ObjectABCInstance: Command):
         if self.GetNumObjects() != 0:
             raise Exception(
-                "Command Trackers can only have one object at a time. Please unload the current Command before trying to load another Command."
+                "Command Trackers can only have one Command at a time. Please unload the current Command before trying to load another Command."
             )
 
         return super().ManualLoad(ObjectABCInstance)
