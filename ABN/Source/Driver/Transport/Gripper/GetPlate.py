de9b446cdc0e129aa8cf27611e989acc7a1b39fb
Bradley Bare <BRadley.Bare@pfizer.com>
2022-11-22 10:36:52 -0600

    I transport done. Now to work on the Hamilton side.

---
+++ ABN/Source/Driver/Transport/Gripper/GetPlate.py
@@ -0,0 +1,17 @@
+from ...Tools.Command.Command import Command
+from .Options.GetPlateOptions.GetPlateOptions import GetPlateOptions
+
+
+class GetPlateCommand(Command):
+    def __init__(self, OptionsInstance: GetPlateOptions):
+        Command.__init__(self)
+        self.OptionsInstance: GetPlateOptions = OptionsInstance
+
+    def GetModuleName(self) -> str:
+        return "Transport Gripper"
+
+    def GetCommandName(self) -> str:
+        return "Get Plate"
+
+    def GetCommandParameters(self) -> dict[str, any]:  # type: ignore
+        return vars(self.OptionsInstance)
---
+++ ABN/Source/Driver/Transport/Gripper/PlacePlate.py
@@ -0,0 +1,17 @@
+from ...Tools.Command.Command import Command
+from .Options.PlacePlateOptions.PlacePlateOptions import PlacePlateOptions
+
+
+class GetPlateCommand(Command):
+    def __init__(self, OptionsInstance: PlacePlateOptions):
+        Command.__init__(self)
+        self.OptionsInstance: PlacePlateOptions = OptionsInstance
+
+    def GetModuleName(self) -> str:
+        return "Transport Gripper"
+
+    def GetCommandName(self) -> str:
+        return "Place Plate"
+
+    def GetCommandParameters(self) -> dict[str, any]:  # type: ignore
+        return vars(self.OptionsInstance)
