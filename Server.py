7339739b4020646ce19fbd9cbcd2cb4ab825758e
Bradley Bare <BRadley.Bare@pfizer.com>
2022-11-22 20:51:16 -0600

    Added Handlers to main file. Personally I like this soooo much more.

--- Server.py
+++ Server.py
@@ -2,21 +2,9 @@ import os
 
 import web
 
-# import ABN.Source.API.Endpoints.AvailableMethods as AvailableMethods
-# import ABN.Source.API.Endpoints.CleanupMethodProject as CleanupMethodProject
-# import ABN.Source.API.Endpoints.Close as Close
-# import ABN.Source.API.Endpoints.Dequeue as Dequeue
-# import ABN.Source.API.Endpoints.GenerateMethodFile as GenerateMethodFile
-# import ABN.Source.API.Endpoints.ListQueue as ListQueue
-# import ABN.Source.API.Endpoints.Open as Open
-# import ABN.Source.API.Endpoints.Queue as Queue
-# import ABN.Source.API.Endpoints.Status as Status
-import ABN.Source.Driver.Endpoints.Request as Request
-import ABN.Source.Driver.Endpoints.Respond as Respond
-import ABN.Source.Driver.Endpoints.IsReady as IsReady
-import ABN.Source.Server.Endpoints.IsActive as IsActive
-import ABN.Source.Server.Endpoints.Kill as Kill
+import ABN.Source.Driver.Handler.DriverHandler as DH
 import ABN.Source.Server.Globals.Logger as Logger
+import ABN.Source.Server.Handler.ServerHandler as SH
 
 if __name__ == "__main__":
 
@@ -25,21 +13,14 @@ if __name__ == "__main__":
 
     Logger.LOG.info("Starting Server")
 
+    ServerHandler = SH.ServerHandler()
+    DriverHandler = DH.DriverHandler()
+
     urls = ()
-    urls += IsReady.urls
-    urls += Respond.urls
-    urls += Request.urls
-    urls += IsActive.urls
-    urls += Kill.urls
-    #    urls += Status.urls
-    #    urls += Queue.urls
-    #    urls += Dequeue.urls
-    #    urls += AvailableMethods.urls
-    #    urls += GenerateMethodFile.urls
-    #    urls += Open.urls
-    #    urls += Close.urls
-    #    urls += ListQueue.urls
-    #    urls += CleanupMethodProject.urls
+    urls += ServerHandler.GetEndpoints()
+    urls += DriverHandler.GetEndpoints()
+
+    ServerHandler.RegisterServerHandler(DriverHandler)
 
     app = web.application(urls, globals())
     app.run()
