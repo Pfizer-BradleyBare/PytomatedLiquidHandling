299263c193216eaa3dfeacbf31f7d479f7f072f7
Bradley Bare <BRadley.Bare@pfizer.com>
2022-11-28 14:55:34 -0600

    Removed Old Interface stuff.

--- ABN/Source/HAL/Pipette/PipetteInterface/Options/TipOptions/TipOptionsTracker.py
+++
@@ -1,6 +0,0 @@
-from ......Tools.AbstractClasses import TrackerABC
-from .TipOptions import TipOptions
-
-
-class TipOptionsTracker(TrackerABC[TipOptions]):
-    pass
--- ABN/Source/HAL/Pipette/PipetteInterface/Options/TransferOptions/TransferOptionsTracker.py
+++
@@ -1,6 +0,0 @@
-from ......Tools.AbstractClasses import TrackerABC
-from .TransferOptions import TransferOptions
-
-
-class TransferOptionsTracker(TrackerABC[TransferOptions]):
-    pass
