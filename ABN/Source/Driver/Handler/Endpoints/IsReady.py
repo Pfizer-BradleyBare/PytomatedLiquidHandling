fadec49fa711bbffe497d5884826629dbe087032
Bradley Bare <BRadley.Bare@pfizer.com>
2022-11-28 21:09:19 -0600

    Had to unrefactor

--- ABN/Source/Driver/Handler/Endpoints/IsReady.py
+++ ABN/Source/Driver/Handler/Endpoints/IsReady.py
@@ -4,6 +4,11 @@ import web
 
 from ....Server.Globals.HandlerRegistry import HandlerRegistry
 from ....Server.Tools.Parser import Parser
+from ...Pipette.Pipette8Channel.Aspirate.Aspirate import AspirateCommand
+from ...Pipette.Pipette8Channel.Aspirate.AspirateOptionsTracker import (
+    AspirateOptions,
+    AspirateOptionsTracker,
+)
 
 urls = ("/Driver/IsReady", "ABN.Source.Driver.Handler.Endpoints.IsReady.IsReady")
 
@@ -19,6 +24,16 @@ class IsReady:
         CommandTrackerInstance = HandlerRegistry.GetObjectByName(
             "Driver Handler"
         ).CommandTrackerInstance  # type:ignore
+
+        Ops = AspirateOptionsTracker()
+
+        Ops.ManualLoad(AspirateOptions("Test", 1, "C1", 1, "1", 1))
+        Ops.ManualLoad(AspirateOptions("Test2", 3, "C3", 1, "1", 3))
+        Ops.ManualLoad(AspirateOptions("Test3", 4, "C4", 1, "1", 4))
+        Ops.ManualLoad(AspirateOptions("Test4", 5, "C5", 1, "1", 5))
+        Ops.ManualLoad(AspirateOptions("Test7", 8, "C8", 1, "1", 8))
+
+        CommandTrackerInstance.ManualLoad(AspirateCommand("Test", Ops))
 
         CommandReady = False
         if CommandTrackerInstance.GetNumObjects() != 0:
