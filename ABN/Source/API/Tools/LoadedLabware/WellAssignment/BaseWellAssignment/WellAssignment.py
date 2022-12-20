3f3b6df4ac16f6074a26353cb333afc08c4dfc1b
Bradley Bare <BRadley.Bare@pfizer.com>
2022-12-20 12:54:52 -0600

    Update

--- ABN/Source/API/Tools/LoadedLabware/WellAssignment/BaseWellAssignment/WellAssignment.py
+++ ABN/Source/API/Tools/LoadedLabware/WellAssignment/BaseWellAssignment/WellAssignment.py
@@ -5,7 +5,7 @@ class WellAssignment(ObjectABC):
     def __init__(self, PhysicalWellNumber: int, MethodName: str, LabwareName: str):
         self.PhysicalWellNumber: int = PhysicalWellNumber
         self.Assignment: str = MethodName + " - " + LabwareName
-        self.ActualVolume: float = 0
+        self.MeasuredVolume: float = 0
 
     def GetName(self) -> int:
         return self.PhysicalWellNumber
@@ -13,5 +13,8 @@ class WellAssignment(ObjectABC):
     def GetAssignment(self) -> str:
         return self.Assignment
 
-    def GetActualVolume(self) -> float:
-        return self.ActualVolume
+    def GetMeasuredVolume(self) -> float:
+        return self.MeasuredVolume
+
+    def UpdateMeasuredVolume(self, NewVolume: float):
+        self.MeasuredVolume = NewVolume
