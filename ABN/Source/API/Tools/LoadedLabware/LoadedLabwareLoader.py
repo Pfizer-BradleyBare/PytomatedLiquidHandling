852188d43cc5278b44bb0a23c4784b7565086b42
Bradley Bare <BRadley.Bare@pfizer.com>
2022-12-20 12:38:30 -0600

    Added actual volume as part of the well assignment. Beautiful LOL

---
+++ ABN/Source/API/Tools/LoadedLabware/LoadedLabwareLoader.py
@@ -0,0 +1,8 @@
+def load():
+    ...
+
+    # First is there labware on deck that can be loaded into?
+
+    # If not, we need to ask the user to load that labware. But is there enough deck space available?
+
+    # once we confirm the labware is available on the deck then we can assign the wells
--- ABN/Source/API/Tools/LoadedLabware/WellAssignment/BaseWellAssignment/WellAssignment.py
+++ ABN/Source/API/Tools/LoadedLabware/WellAssignment/BaseWellAssignment/WellAssignment.py
@@ -5,9 +5,13 @@ class WellAssignment(ObjectABC):
     def __init__(self, PhysicalWellNumber: int, MethodName: str, LabwareName: str):
         self.PhysicalWellNumber: int = PhysicalWellNumber
         self.Assignment: str = MethodName + " - " + LabwareName
+        self.ActualVolume: float = 0
 
     def GetName(self) -> int:
         return self.PhysicalWellNumber
 
     def GetAssignment(self) -> str:
         return self.Assignment
+
+    def GetActualVolume(self) -> float:
+        return self.ActualVolume
