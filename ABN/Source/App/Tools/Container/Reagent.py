52a1c7425d7b9dcc1c442240f110c5a03d294b1a
Bradley Bare <BRadley.Bare@pfizer.com>
2022-12-21 13:54:39 -0600

    Forget Index start. Added

--- ABN/Source/App/Tools/Container/Reagent.py
+++ ABN/Source/App/Tools/Container/Reagent.py
@@ -30,8 +30,8 @@ class Reagent(APIReagent):
             self.ExcelInstance.SelectSheet("Solutions")
 
         return LiquidClassCategory(
-            self.ExcelInstance.ReadCellValue(4, 2),
-            self.ExcelInstance.ReadCellValue(5, 2),
-            self.ExcelInstance.ReadCellValue(6, 2),
-            self.ExcelInstance.ReadCellValue(7, 2),
+            self.ExcelInstance.ReadCellValue(self.Row + 4, self.Col + 2),
+            self.ExcelInstance.ReadCellValue(self.Row + 5, self.Col + 2),
+            self.ExcelInstance.ReadCellValue(self.Row + 6, self.Col + 2),
+            self.ExcelInstance.ReadCellValue(self.Row + 7, self.Col + 2),
         )
