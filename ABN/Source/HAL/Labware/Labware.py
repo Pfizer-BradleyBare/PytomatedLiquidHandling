4e024a7ea5b21095c9f84411faba26d5b4651b7c
Bradley Bare <BRadley.Bare@pfizer.com>
2023-01-19 13:42:56 -0600

    Moved labware calculator into labware object.

--- ABN/Source/HAL/Labware/Labware.py
+++ ABN/Source/HAL/Labware/Labware.py
@@ -18,3 +18,66 @@ class Labware(ObjectABC):
 
     def GetName(self) -> str:
         return self.Name
+
+    def GetWellHeightFromVolume(self, Volume: float) -> float:
+        CalculatedHeight = 0.0
+
+        if self.LabwareWells is None:
+            raise Exception(
+                "Labware Wells is none. You can only use this function on labware with wells."
+            )
+
+        WellsEquations = (
+            self.LabwareWells.WellEquationTrackerInstance.GetObjectsAsList()
+        )
+
+        while True:
+            TempHeight = CalculatedHeight
+            CalculatedVolume = 0
+            # reset each round
+
+            for Segment in WellsEquations:
+                SegmentHeight = Segment.Height
+                EvalHeight = TempHeight
+
+                if EvalHeight > SegmentHeight:
+                    EvalHeight = SegmentHeight
+                # Make sure we do not exceed the segment height during the calc
+
+                CalculatedVolume += eval(Segment.Equation, {}, {"h": EvalHeight})
+                TempHeight -= SegmentHeight
+
+                if TempHeight <= 0:
+                    break
+
+            if CalculatedVolume >= Volume or TempHeight > 0:
+                break
+
+            CalculatedHeight += 0.1
+
+        return CalculatedHeight
+
+    def GetWellVolumeFromHeight(self, Height: float) -> float:
+        if self.LabwareWells is None:
+            raise Exception(
+                "Labware Wells is none. You can only use this function on labware with wells."
+            )
+
+        WellsEquations = (
+            self.LabwareWells.WellEquationTrackerInstance.GetObjectsAsList()
+        )
+        CalculatedVolume = 0
+
+        for Segment in WellsEquations:
+            SegmentHeight = Segment.Height
+
+            if Height > SegmentHeight:
+                EvalHeight = SegmentHeight
+            else:
+                EvalHeight = Height
+
+            Height -= SegmentHeight
+
+            CalculatedVolume += eval(Segment.Equation, {}, {"h": EvalHeight})
+
+        return CalculatedVolume
