4ff15aea3c9828b3cd0106fa9e6fba859041d9ed
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-03-07 11:29:30 -0600

    n/a

--- App.py
+++ App.py
@@ -2,9 +2,6 @@ import sys
 
 sys.dont_write_bytecode = True
 
-import os
-from logging import DEBUG
-
 from App.Handler import Handler
 from PytomatedLiquidHandling.Tools.Logger import GenerateLogFilePath, Logger
 
