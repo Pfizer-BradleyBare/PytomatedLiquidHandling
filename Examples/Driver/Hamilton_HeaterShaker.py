7571b36d6275a02df5bcd62acf8cda645178fc23
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-03-24 19:07:00 -0500

    Created Example File

--- Examples/Driver/Hamilton_HeaterShaker.py
+++ Examples/Driver/Hamilton_HeaterShaker.py
@@ -1,1 +1,52 @@
-from PytomatedLiquidHandling import Driver
+from PytomatedLiquidHandling import Logger, Driver
+from PytomatedLiquidHandling.Driver.TemperatureControl import HeaterShaker
+import logging
+import os
+import time
+
+LoggerInstance = Logger(
+    "MyLogger", logging.DEBUG, os.path.join(os.path.dirname(__file__), "Logging")
+)
+DriverHandlerInstance = Driver.Handler(LoggerInstance)
+# Creates the handler so we can communicate with the Hamilton
+
+ConnectCommand = HeaterShaker.Connect.Command(HeaterShaker.Connect.Options(1), False)
+ConnectCommand.Execute()
+HeaterShakerHandleId = ConnectCommand.GetHandleID()
+# Connect and get our Handle
+
+DesiredTemperature = 37
+StartTempCommand = HeaterShaker.StartTemperatureControl.Command(
+    HeaterShaker.StartTemperatureControl.Options(
+        HeaterShakerHandleId, DesiredTemperature
+    ),
+    False,
+)
+StartTempCommand.Execute()
+# Turn on the Heat
+
+TemperatureOffset = 2
+while True:
+    time.sleep(10)
+    GetTempCommand = HeaterShaker.GetTemperature.Command(
+        HeaterShaker.GetTemperature.Options(HeaterShakerHandleId), False
+    )
+    GetTempCommand.Execute()
+
+    CurrentTemperature = GetTempCommand.GetTemperature()
+
+    if (
+        DesiredTemperature - TemperatureOffset
+        <= CurrentTemperature
+        <= DesiredTemperature + TemperatureOffset
+    ):
+        break
+# Wait for temperature to fall within desired range
+
+HeaterShaker.StopTemperatureControl.Command(
+    HeaterShaker.StopTemperatureControl.Options(HeaterShakerHandleId), False
+).Execute()
+# Turn off heat
+
+DriverHandlerInstance.KillServer()
+# Done!
