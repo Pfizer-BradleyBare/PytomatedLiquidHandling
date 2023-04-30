1f44566f3c19e156205b3b91ca6f46692c411349
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-04-30 13:35:10 -0500

    Multichannel and single channel now done

--- PytomatedLiquidHandling/Driver/Pipette/SingleChannel/Dispense/Options.py
+++ PytomatedLiquidHandling/Driver/Pipette/SingleChannel/Dispense/Options.py
@@ -4,30 +4,46 @@ from .....Tools.AbstractClasses import NonUniqueObjectABC
 
 
 class AdvancedOptions:
-    def __init(self):
-        self.Mode: int = 0
-        self.FixHeightFromBottom: float = 0
-        self.RetractDistanceForTransportAir: float = 0
-        self.CapacitiveLiquidLevelDetection: int = 0
-        self.SubmergeDepth: float = 2
-        self.SideTouch: int = 0
+    def __init__(
+        self,
+        *,
+        Mode: int | None = None,
+        FixHeightFromBottom: float | None = None,
+        RetractDistanceForTransportAir: float | None = None,
+        CapacitiveLiquidLevelDetection: int | None = None,
+        SubmergeDepth: float | None = None,
+        SideTouch: int | None = None,
+        LiquidFollowing: int | None = None,
+        MixCycles: int | None = None,
+        MixPosition: float | None = None,
+        MixVolume: float | None = None,
+    ):
+        self.Mode: int | None = Mode
+        self.FixHeightFromBottom: float | None = FixHeightFromBottom
+        self.RetractDistanceForTransportAir: float | None = (
+            RetractDistanceForTransportAir
+        )
+        self.CapacitiveLiquidLevelDetection: int | None = CapacitiveLiquidLevelDetection
+        self.SubmergeDepth: float | None = SubmergeDepth
+        self.SideTouch: int | None = SideTouch
 
-        self.LiquidFollowing: int = 0
-        self.MixCycles: int = 0
-        self.MixPosition: float = 0
-        self.MixVolume: float = 0
+        self.LiquidFollowing: int | None = LiquidFollowing
+        self.MixCycles: int | None = MixCycles
+        self.MixPosition: float | None = MixPosition
+        self.MixVolume: float | None = MixVolume
 
 
 class Options(NonUniqueObjectABC):
     def __init__(
         self,
+        *,
         ChannelNumber: int,
         Sequence: str,
         SequencePosition: int,
         LiquidClass: str,
         Volume: float,
+        AdvancedOptionsInstance: AdvancedOptions = AdvancedOptions(),
     ):
-
         self.ChannelNumber: int = ChannelNumber
 
         self.Sequence: str = Sequence
@@ -37,4 +53,21 @@ class Options(NonUniqueObjectABC):
 
         self.LiquidClass: str = LiquidClass
 
-        self.Advanced: AdvancedOptions = AdvancedOptions()
+        self.AdvancedOptionsInstance: AdvancedOptions = AdvancedOptions(
+            Mode=0,
+            FixHeightFromBottom=0,
+            RetractDistanceForTransportAir=0,
+            CapacitiveLiquidLevelDetection=0,
+            SubmergeDepth=2,
+            SideTouch=0,
+            LiquidFollowing=0,
+            MixCycles=0,
+            MixPosition=0,
+            MixVolume=0,
+        )
+        # These are the default advanced values
+
+        self.AdvancedOptionsInstance.__dict__.update(
+            {k: v for k, v in vars(AdvancedOptionsInstance) if v is not None}
+        )
+        # This is used to update the values from the user if the user decided to change any advanced settings
--- PytomatedLiquidHandling/Driver/Pipette/SingleChannel/Eject/Options.py
+++ PytomatedLiquidHandling/Driver/Pipette/SingleChannel/Eject/Options.py
@@ -9,10 +9,22 @@ class AdvancedOptions:
 
 
 class Options(NonUniqueObjectABC):
-    def __init__(self, Sequence: str, ChannelNumber: int, SequencePosition: int):
-
+    def __init__(
+        self,
+        *,
+        Sequence: str,
+        ChannelNumber: int,
+        SequencePosition: int,
+        AdvancedOptionsInstance: AdvancedOptions = AdvancedOptions()
+    ):
         self.ChannelNumber: int = ChannelNumber
         self.Sequence: str = Sequence
         self.SequencePosition: int = SequencePosition
 
-        self.Advanced: AdvancedOptions = AdvancedOptions()
+        self.AdvancedOptionsInstance: AdvancedOptions = AdvancedOptions()
+        # These are the default advanced values
+
+        self.AdvancedOptionsInstance.__dict__.update(
+            {k: v for k, v in vars(AdvancedOptionsInstance) if v is not None}
+        )
+        # This is used to update the values from the user if the user decided to change any advanced settings
