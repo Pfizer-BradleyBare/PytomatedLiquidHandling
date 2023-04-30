bfdf03bbdc58e90ca3a4f604bb9f441c61c2535f
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-04-30 16:27:26 -0500

    TImer and Tips done.

--- PytomatedLiquidHandling/Driver/Tip/NTR/LoadTips/Options.py
+++ PytomatedLiquidHandling/Driver/Tip/NTR/LoadTips/Options.py
@@ -1,11 +1,30 @@
 from .....Tools.AbstractClasses import NonUniqueObjectABC
 
 
-class Options(NonUniqueObjectABC):
-    def __init__(self, TipSequence: str, RackWasteSequence: str, GripperSequence: str):
+class AdvancedOptions:
+    def __init__(self, *, MinimumTips: int | None = None):
+        self.MinimumTips: int | None = MinimumTips
 
+
+class Options(NonUniqueObjectABC):
+    def __init__(
+        self,
+        *,
+        TipSequence: str,
+        RackWasteSequence: str,
+        GripperSequence: str,
+        AdvancedOptionsInstance: AdvancedOptions = AdvancedOptions(),
+    ):
         self.TipSequence: str = TipSequence
         self.RackWasteSequence: str = RackWasteSequence
         self.GripperSequence: str = GripperSequence
 
         self.LoadingText: str = "Load NTR Tips"
+
+        self.AdvancedOptionsInstance: AdvancedOptions = AdvancedOptions(MinimumTips=0)
+        # These are the default advanced values
+
+        self.AdvancedOptionsInstance.__dict__.update(
+            {k: v for k, v in vars(AdvancedOptionsInstance) if v is not None}
+        )
+        # This is used to update the values from the user if the user decided to change any advanced settings
