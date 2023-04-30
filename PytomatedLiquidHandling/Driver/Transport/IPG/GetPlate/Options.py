b659649c14e214881b15fac3865078313926241c
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-04-30 16:41:24 -0500

    Transport devices done

--- PytomatedLiquidHandling/Driver/Transport/IPG/GetPlate/Options.py
+++ PytomatedLiquidHandling/Driver/Transport/IPG/GetPlate/Options.py
@@ -1,29 +1,66 @@
 from .....Tools.AbstractClasses import NonUniqueObjectABC
 
 
+class AdvancedOptions:
+    def __init__(
+        self,
+        *,
+        GripHeight: float | None = None,
+        GripMode: int | None = None,
+        Movement: int | None = None,
+        RetractDistance: float | None = None,
+        LiftupHeight: float | None = None,
+        LabwareOrientation: int | None = None,
+        GripForce: int | None = None,
+        Tolerance: float | None = None,
+        InverseGrip: int | None = None,
+        CollisionControl: int | None = None,
+    ):
+        self.GripHeight: float | None = GripHeight
+        self.GripMode: int | None = GripMode
+
+        self.Movement: int | None = Movement
+
+        # Only matters if movement is 1
+        self.RetractDistance: float | None = RetractDistance
+        self.LiftupHeight: float | None = LiftupHeight
+        self.LabwareOrientation: int | None = LabwareOrientation
+
+        self.GripForce: int | None = GripForce
+        self.Tolerance: float | None = Tolerance
+        self.InverseGrip: int | None = InverseGrip
+        self.CollisionControl: int | None = CollisionControl
+
+
 class Options(NonUniqueObjectABC):
     def __init__(
         self,
+        *,
         PlateSequence: str,
         GripWidth: float,
         OpenWidth: float,
+        AdvancedOptionsInstance: AdvancedOptions = AdvancedOptions(),
     ):
-
         self.PlateSequence: str = PlateSequence
 
-        self.GripHeight: float = 3
-        self.GripMode: int = 0
         self.GripWidth: float = GripWidth
         self.OpenWidth: float = OpenWidth
 
-        self.Movement: int = 0
+        self.AdvancedOptionsInstance: AdvancedOptions = AdvancedOptions(
+            GripHeight=3,
+            GripMode=0,
+            Movement=0,
+            RetractDistance=0,
+            LiftupHeight=0,
+            LabwareOrientation=1,
+            GripForce=4,
+            Tolerance=2,
+            InverseGrip=0,
+            CollisionControl=1,
+        )
+        # These are the default advanced values
 
-        # Only matters if movement is 1
-        self.RetractDistance: float = 0
-        self.LiftupHeight: float = 0
-        self.LabwareOrientation: int = 1
-
-        self.GripForce: int = 5
-        self.Tolerance: float = 2
-        self.InverseGrip: int = 0
-        self.CollisionControl: int = 1
+        self.AdvancedOptionsInstance.__dict__.update(
+            {k: v for k, v in vars(AdvancedOptionsInstance) if v is not None}
+        )
+        # This is used to update the values from the user if the user decided to change any advanced settings
--- PytomatedLiquidHandling/Driver/Transport/IPG/PlacePlate/Options.py
+++ PytomatedLiquidHandling/Driver/Transport/IPG/PlacePlate/Options.py
@@ -1,19 +1,45 @@
 from .....Tools.AbstractClasses import NonUniqueObjectABC
 
 
+class AdvancedOptions:
+    def __init__(
+        self,
+        *,
+        Movement: int | None = None,
+        RetractDistance: float | None = None,
+        LiftupHeight: float | None = None,
+        LabwareOrientation: int | None = None,
+        CollisionControl: int | None = None,
+    ):
+        self.Movement: int | None = Movement
+
+        # Only matters if movement is 1
+        self.RetractDistance: float | None = RetractDistance
+        self.LiftupHeight: float | None = LiftupHeight
+        self.LabwareOrientation: int | None = LabwareOrientation
+
+        self.CollisionControl: int | None = CollisionControl
+
+
 class Options(NonUniqueObjectABC):
     def __init__(
         self,
+        *,
         PlateSequence: str,
+        AdvancedOptionsInstance: AdvancedOptions = AdvancedOptions(),
     ):
-
         self.PlateSequence: str = PlateSequence
 
-        self.Movement: int = 0
+        self.AdvancedOptionsInstance: AdvancedOptions = AdvancedOptions(
+            Movement=0,
+            RetractDistance=0,
+            LiftupHeight=0,
+            LabwareOrientation=1,
+            CollisionControl=1,
+        )
+        # These are the default advanced values
 
-        # Only matters if movement is 1
-        self.RetractDistance: float = 0
-        self.LiftupHeight: float = 0
-        self.LabwareOrientation: int = 1
-
-        self.CollisionControl: int = 1
+        self.AdvancedOptionsInstance.__dict__.update(
+            {k: v for k, v in vars(AdvancedOptionsInstance) if v is not None}
+        )
+        # This is used to update the values from the user if the user decided to change any advanced settings
