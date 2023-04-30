22d930547d609cd37f6d8ddea6d0a52e7caea1f5
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-04-30 13:41:20 -0500

    FlipTube Finished

--- PytomatedLiquidHandling/Driver/ClosedContainer/FlipTube/Close/__init__.py
+++ PytomatedLiquidHandling/Driver/ClosedContainer/FlipTube/Close/__init__.py
@@ -1,5 +1,5 @@
 from .Command import Command
-from .Options import Options
+from .Options import Options, AdvancedOptions
 from .OptionsTracker import OptionsTracker
 
 __all__ = ["Command", "Options", "OptionsTracker"]
