c8f98a3614eaa53ccad69a094be53d05f8426057
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-05-09 00:27:54 -0500

    Removed autoload. Part of deck loading dialog

--- PytomatedLiquidHandling/Driver/Hamilton/Autoload/Move/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Command import Command
-from .Options import Options, AdvancedOptions
-
-__all__ = ["Command", "Options"]
--- PytomatedLiquidHandling/Driver/Hamilton/Autoload/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import LoadCarrier, Move, UnloadCarrier
-
-__all__ = ["LoadCarrier", "Move", "UnloadCarrier"]
