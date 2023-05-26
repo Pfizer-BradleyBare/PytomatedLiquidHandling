4a65af66612e4a2968dbdd1d8affa9fcf41c32a6
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-05-26 14:54:10 -0500

    Start to add backend as part of the HAL

--- PytomatedLiquidHandling/HAL/Tools/AbstractInterface/InterfaceABC.py
+++ PytomatedLiquidHandling/HAL/Tools/AbstractInterface/InterfaceABC.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
 
+from ....Driver.Tools.AbstractClasses import BackendABC
+
 
 class InterfaceABC(ABC):
-    def __init__(self, CustomErrorHandling: bool):
+    def __init__(self, BackendInstance: BackendABC,CustomErrorHandling: bool):
+        self.BackendInstance: BackendABC = BackendInstance
         self.CustomErrorHandling: bool = CustomErrorHandling
 
     @abstractmethod
