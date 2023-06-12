6bd755a9d56f92b4c9f516331b805d884ce63986
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-06-12 11:33:05 -0500

    Made the backend tracker

--- PytomatedLiquidHandling/HAL/Backend/BackendLoader.py
+++ PytomatedLiquidHandling/HAL/Backend/BackendLoader.py
@@ -1,7 +1,38 @@
 import yaml
+
+from ...Driver.Hamilton.Backend import MicrolabStarBackend, VantageBackend
+from ...Driver.UnchainedLabs.Backend import StunnerBackend
+from ...Tools.Logger import Logger
 from .BackendTracker import BackendTracker
-from ...Driver.Hamilton.Backend import VantageBackend, MicrolabStarBackend
 
 
-def LoadYaml(FilePath: str) -> BackendTracker:
-    ...
+def LoadYaml(LoggerInstance: Logger, FilePath: str) -> BackendTracker:
+    BackendTrackerInstance = BackendTracker()
+
+    FileHandle = open(FilePath, "r")
+    ConfigFile = yaml.full_load(FileHandle)
+    FileHandle.close()
+    # Get config file contents
+
+    for DeviceID in ConfigFile:
+        Device = ConfigFile[DeviceID]
+        UniqueID = Device["Unique Identifier"]
+
+        if DeviceID == "Microlab Star":
+            DeviceInstance = MicrolabStarBackend(
+                UniqueID, LoggerInstance, Device["Deck Layout Path"]
+            )
+        elif DeviceID == "Vantage":
+            DeviceInstance = VantageBackend(
+                UniqueID, LoggerInstance, Device["Deck Layout Path"]
+            )
+        elif DeviceID == "Stunner":
+            DeviceInstance = StunnerBackend(
+                UniqueID, LoggerInstance, Device["IP Address"], Device["Port"]
+            )
+        else:
+            raise Exception("Device not recognized")
+
+        BackendTrackerInstance.LoadSingle(DeviceInstance)
+
+    return BackendTrackerInstance
