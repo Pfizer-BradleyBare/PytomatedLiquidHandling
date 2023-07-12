c0adc38991de3d49018ff1f4c14297f1f4c9faec
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-07-12 16:42:01 -0500

    More interface functions in step

--- PytomatedLiquidHandling/API/Scheduler/Method/Step/StepABC.py
+++ PytomatedLiquidHandling/API/Scheduler/Method/Step/StepABC.py
@@ -9,7 +9,25 @@ from PytomatedLiquidHandling.Tools.AbstractClasses import UniqueObjectABC
 @dataclass
 class StepABC(UniqueObjectABC):
     @abstractmethod
+    def PreExecute(
+        self,
+        Simulate: bool,
+        HALInstance: HAL,
+        ResourceReservationTrackerInstance: ResourceReservation.ResourceReservationTracker,
+    ):
+        ...
+
+    @abstractmethod
     def Execute(
+        self,
+        Simulate: bool,
+        HALInstance: HAL,
+        ResourceReservationTrackerInstance: ResourceReservation.ResourceReservationTracker,
+    ):
+        ...
+
+    @abstractmethod
+    def PostExecute(
         self,
         Simulate: bool,
         HALInstance: HAL,
