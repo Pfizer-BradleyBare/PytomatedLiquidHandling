df7838c542e8cc9bd6bf474bdabaa2c4f4aabdea
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-07-13 14:45:24 -0500

    Saving state here. Small refactor incomming.

--- PytomatedLiquidHandling/API/Scheduler/Method/MethodABC.py
+++ PytomatedLiquidHandling/API/Scheduler/Method/MethodABC.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from enum import Enum
+from typing import Type
 
 import treelib
 
@@ -24,8 +25,8 @@ class MethodABC(UniqueObjectABC):
     class StateOptions(Enum):
         Queued = 0
         Running = 1
-        Waiting = 2
-        Error = 3
+        Error = 2
+        Complete = 3
 
     State: StateOptions = field(init=False, default=StateOptions.Queued)
 
@@ -59,9 +60,82 @@ class MethodABC(UniqueObjectABC):
             self.MethodStepPathways.append(StepTrackerInstance)
         # Take the step tree and create discrete lists of step for each pathway.
 
-    def ExecuteStep(
+    def PreExecute(
         self,
         HALInstance: HAL,
         ResourceReservationTrackerInstance: ResourceReservation.ResourceReservationTracker,
     ):
-        ...
+        for StepTrackerInstance in self.MethodStepPathways:
+            for StepInstance in StepTrackerInstance.GetObjectsAsList():
+                if StepInstance.ExecuteComplete == True:
+                    if (
+                        self.ExecutedStepTrackerInstance.IsTracked(
+                            StepInstance.UniqueIdentifier
+                        )
+                        == False
+                    ):
+                        self.ExecutedStepTrackerInstance.LoadSingle(StepInstance)
+            # add to executed tracker
+
+            for StepInstance in StepTrackerInstance.GetObjectsAsList():
+                if (
+                    self.ExecutedStepTrackerInstance.IsTracked(
+                        StepInstance.UniqueIdentifier
+                    )
+                    == True
+                ):
+                    StepTrackerInstance.UnloadSingle(StepInstance)
+            # remove from each list.
+
+            ExecutionBlocks: dict[str | int, list[Type[StepABC]]] = dict()
+            for StepInstance in StepTrackerInstance.GetObjectsAsList():
+                ExecutionBlocks[
+                    StepInstance.UniqueIdentifier
+                ] = StepInstance.StepPreExecuteBlocks
+            # get our execution blocks
+
+            TimeToStep = 0
+            for StepInstance in StepTrackerInstance.GetObjectsAsList():
+                StepBlocks = sum(
+                    [
+                        ExecutionBlocks[Key]
+                        for Key in ExecutionBlocks
+                        if Key != StepInstance.UniqueIdentifier
+                    ],
+                    [],
+                )
+
+                if type(StepInstance) in StepBlocks:
+                    continue
+
+                TimeToStep += StepInstance.PreExecuteTime() + StepInstance.ExecuteTime()
+
+                StepInstance.PreExecute(
+                    self.Simulate,
+                    HALInstance,
+                    ResourceReservationTrackerInstance,
+                    TimeToStep,
+                )
+            # do the EQ
+        # first we need to try to do our pre-execution for as many steps as possible.
+        # We always iterate over all steps to find PreExecutions that are meant to run during timer countdowns
+
+    def Execute(
+        self,
+        HALInstance: HAL,
+        ResourceReservationTrackerInstance: ResourceReservation.ResourceReservationTracker,
+    ):
+        TimeKeeper = float("inf")
+        FastestPathway: StepTracker | None = None
+        for StepTrackerInstance in self.MethodStepPathways:
+            PathwayTime = 0
+            for StepInstance in StepTrackerInstance.GetObjectsAsList():
+                if StepInstance.HasTimer() == True:
+                    break
+                PathwayTime += (
+                    StepInstance.PreExecuteTime() + StepInstance.ExecuteTime()
+                )
+
+            if PathwayTime < TimeKeeper:
+                FastestPathway = StepTrackerInstance
+        # find the fastest path
--- PytomatedLiquidHandling/API/Scheduler/Method/Step/StepABC.py
+++ PytomatedLiquidHandling/API/Scheduler/Method/Step/StepABC.py
@@ -10,19 +10,36 @@ from PytomatedLiquidHandling.Tools.AbstractClasses import UniqueObjectABC
 @dataclass
 class StepABC(UniqueObjectABC):
     AllStepClasses: ClassVar[list[Type[Self]]] = field(init=False, default_factory=list)
+
+    StepPreExecuteBlocks: list[Type[Self]] = field(init=False, default_factory=list)
+    StepExecuteBlocks: list[Type[Self]] = field(init=False, default_factory=list)
+
     PreExecuteComplete: bool = field(init=False, default=False)
-    StepExecutionBlock: list[Type[Self]] = field(init=False, default_factory=list)
+    ExecuteComplete: bool = field(init=False, default=False)
 
     def __init_subclass__(cls) -> None:
         StepABC.AllStepClasses.append(cls)
+
+    @abstractmethod
+    def HasTimer(self) -> bool:
+        ...
+
+    @abstractmethod
+    def PreExecuteTime(self) -> float:
+        ...
 
     def PreExecute(
         self,
         Simulate: bool,
         HALInstance: HAL,
         ResourceReservationTrackerInstance: ResourceReservation.ResourceReservationTracker,
+        TimeTillExecution: float,
     ):
         self.PreExecuteComplete = True
+
+    @abstractmethod
+    def ExecuteTime(self) -> float:
+        ...
 
     @abstractmethod
     def Execute(
