c8489d676fa83224283cbb15849f171149019842
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-07-20 06:28:27 -0500

    Commenting

--- PytomatedLiquidHandling/API/Scheduler/Method/Step/TaskABC.py
+++ PytomatedLiquidHandling/API/Scheduler/Method/Step/TaskABC.py
@@ -17,10 +17,25 @@ class TaskABC(UniqueObjectABC):
         Consecutive = 1  # Consequtive means the task CANNOT move.
         AsSoonAsPossible = 2  # As soon as possible means the task will be moved to the beginning of the submethod task queue
 
-    SchedulingSeparator: bool  # This indicates whether the method should be split into submethods at this task
     ExecutionWindow: ExecutionWindows
-    RequiredResources: list[Type[InterfaceABC] | Container]
+    # This is a rule for the scheduler about how tasks must be executed. If ExecutionWindow is NOT Consecutive
+    # then the scheduler is free to move the task around for better scheduling.
+
+    SchedulingSeparator: bool
+    # This indicates whether the method should be split into submethods at this task.
+    # the scheduler works be scheduling a series of tasks that make up a submethod.
+    # splitting a method into submethods make the workflow easier to schedule.
+    # This flag can be considered optional (False) for critical method workflows but understand
+    # parallel scheduling may be nonexistant.
+
     MinExecutionTime: float
+    # if 0 then the scheduler will assume this task is never-ending.
+    # This is useful for unknown duration timers, user input, and unexpected errors.
+    # post user input or unknown duration the min execution time should
+    # be modified to reflect time taken and the scheduler rescheduled.
+
+    RequiredResources: list[Type[InterfaceABC] | Container]
+    # resources required by the task. This can be any HAL derived object or any container.
 
     QueueUponCompletion: list[list] = field(init=False, default_factory=list)
 
