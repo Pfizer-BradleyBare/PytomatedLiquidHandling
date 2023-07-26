d689b2fca7a19d567e054a19c0a309a64a7add63
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-07-26 18:00:03 -0500

    Need labware so we can load container

--- PytomatedLiquidHandling/API/Tools/Container/Container.py
+++ PytomatedLiquidHandling/API/Tools/Container/Container.py
@@ -4,10 +4,10 @@ from PytomatedLiquidHandling.Tools.AbstractClasses import (
     UniqueObjectABC,
     UniqueObjectTrackerABC,
 )
-
+from PytomatedLiquidHandling.HAL import Labware
 from .Well import Well
 
 
 @dataclass
 class Container(UniqueObjectABC, UniqueObjectTrackerABC[Well]):
-    ...
+    LabwareInstance: Labware.PipettableLabware
