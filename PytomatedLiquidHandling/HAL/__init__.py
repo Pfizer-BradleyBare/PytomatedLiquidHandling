57ec473581525531fc7b6bac3e8b9252185ccb6b
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-10-04 13:24:25 -0500

    Removing unneeded classes

--- PytomatedLiquidHandling/HAL/__init__.py
+++ PytomatedLiquidHandling/HAL/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from . import (
     Backend,
     Carrier,
@@ -13,4 +15,75 @@ from . import (
     Tip,
     TransportDevice,
 )
-from .HAL import HAL
+
+ClosedContainers: dict[str, ClosedContainer.Base.ClosedContainerABC]
+# DeckLoaders: dict[str, DeckLoader.Base.DeckLoaderABC]
+LayoutItems: dict[str, LayoutItem.Base.LayoutItemABC]
+# MagneticRacks: dict[str, MagneticRack.Base.MagneticRackABC]
+Pipettes: dict[str, Pipette.Base.PipetteABC]
+StorageDevices: dict[str, StorageDevice.Base.StorageDeviceABC]
+TempControlDevices: dict[str, HeatCoolShakeDevice.Base.HeatCoolShakeDeviceABC]
+TransportDevices: dict[str, TransportDevice.Base.TransportDeviceABC]
+
+
+def Initialize(ConfigFolderPath: str):
+    Backends = Backend.Loader.LoadYaml(os.path.join(ConfigFolderPath, "Backend.yaml"))
+
+    Carriers = Carrier.Loader.LoadYaml(os.path.join(ConfigFolderPath, "Carrier.yaml"))
+
+    Labwares = Labware.Loader.LoadYaml(os.path.join(ConfigFolderPath, "Labware.yaml"))
+
+    global TransportDevices
+    TransportDevices = TransportDevice.Loader.LoadYaml(
+        Backends,
+        Labwares,
+        os.path.join(ConfigFolderPath, "Transport.yaml"),
+    )
+
+    DeckLocations = DeckLocation.Loader.LoadYaml(
+        Carriers,
+        TransportDevices,
+        os.path.join(ConfigFolderPath, "DeckLocation.yaml"),
+    )
+
+    global LayoutItems
+    LayoutItems = LayoutItem.Loader.LoadYaml(
+        Labwares,
+        DeckLocations,
+        os.path.join(ConfigFolderPath, "LayoutItem.yaml"),
+    )
+
+    global ClosedContainers
+    ClosedContainers = ClosedContainer.Loader.LoadYaml(
+        Backends,
+        DeckLocations,
+        Labwares,
+        os.path.join(ConfigFolderPath, "ClosedContainer.yaml"),
+    )
+
+    global TempControlDevices
+    TempControlDevices = HeatCoolShakeDevice.Loader.LoadYaml(
+        Backends,
+        LayoutItems,
+        os.path.join(ConfigFolderPath, "TempControlDevice.yaml"),
+    )
+
+    Tips = Tip.Loader.LoadYaml(
+        Backends,
+        os.path.join(ConfigFolderPath, "Tip.yaml"),
+    )
+
+    global Pipettes
+    Pipettes = Pipette.Loader.LoadYaml(
+        Backends,
+        DeckLocations,
+        Labwares,
+        Tips,
+        os.path.join(ConfigFolderPath, "Pipette.yaml"),
+    )
+
+    global StorageDevices
+    StorageDevices = StorageDevice.Loader.LoadYaml(
+        LayoutItems,
+        os.path.join(ConfigFolderPath, "Storage.yaml"),
+    )
