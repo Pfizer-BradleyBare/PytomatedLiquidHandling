42facda46b13628efe55b796662243797d35998e
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-10-09 09:23:01 -0500

    Just moved function to top

--- PytomatedLiquidHandling/API/DeckManager/Transport.py
+++ PytomatedLiquidHandling/API/DeckManager/Transport.py
@@ -6,6 +6,37 @@ from .Loading import GetLoadedLayoutItems, LoadedWells
 TransitionPoints: dict[str, LayoutItem.Base.LayoutItemABC] = dict()
 
 
+def TransportContainer(
+    Container: Container.Container,
+    AcceptableDeckLocations: list[DeckLocation.Base.DeckLocationABC],
+):
+    ContainerLayoutItems = GetLoadedLayoutItems(Container)
+
+    InUseDeckLocations = [
+        LoadedWell.LayoutItem.DeckLocation for LoadedWell in LoadedWells
+    ]
+
+    PotentialDeckLocations = [
+        DeckLocation
+        for DeckLocation in AcceptableDeckLocations
+        if DeckLocation not in InUseDeckLocations
+    ]
+
+    if len(PotentialDeckLocations) < len(LayoutItems):
+        raise Exception(
+            "There are not enough free deck locations to transport this container."
+        )
+
+    PotentialLayoutItems = [
+        LayoutItem
+        for LayoutItem in LayoutItems.values()
+        if LayoutItem.DeckLocation in PotentialDeckLocations
+    ]
+
+    for Index in range(0, len(ContainerLayoutItems)):
+        TransportLayoutItem(ContainerLayoutItems[Index], PotentialLayoutItems[Index])
+
+
 def TransportLayoutItem(
     SourceLayoutItem: LayoutItem.Base.LayoutItemABC,
     DestinationLayoutItem: LayoutItem.Base.LayoutItemABC,
@@ -60,34 +91,3 @@ def TransportLayoutItem(
         )
     # Hooray we can just do the transport!
     # Are these bad boys compatible? Meaning are the pickup options similar...
-
-
-def TransportContainer(
-    Container: Container.Container,
-    AcceptableDeckLocations: list[DeckLocation.Base.DeckLocationABC],
-):
-    ContainerLayoutItems = GetLoadedLayoutItems(Container)
-
-    InUseDeckLocations = [
-        LoadedWell.LayoutItem.DeckLocation for LoadedWell in LoadedWells
-    ]
-
-    PotentialDeckLocations = [
-        DeckLocation
-        for DeckLocation in AcceptableDeckLocations
-        if DeckLocation not in InUseDeckLocations
-    ]
-
-    if len(PotentialDeckLocations) < len(LayoutItems):
-        raise Exception(
-            "There are not enough free deck locations to transport this container."
-        )
-
-    PotentialLayoutItems = [
-        LayoutItem
-        for LayoutItem in LayoutItems.values()
-        if LayoutItem.DeckLocation in PotentialDeckLocations
-    ]
-
-    for Index in range(0, len(ContainerLayoutItems)):
-        TransportLayoutItem(ContainerLayoutItems[Index], PotentialLayoutItems[Index])
