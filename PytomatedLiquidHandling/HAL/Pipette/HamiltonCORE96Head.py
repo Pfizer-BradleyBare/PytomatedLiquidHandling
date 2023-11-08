786f76b8d09430292cc8735879fd8429667d11ae
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-11-08 17:14:21 -0600

    updated but not finished.

--- PytomatedLiquidHandling/HAL/Pipette/HamiltonCORE96Head.py
+++ PytomatedLiquidHandling/HAL/Pipette/HamiltonCORE96Head.py
@@ -10,78 +10,71 @@ class HamiltonCORE96Head(PipetteABC):
     Backend: HamiltonBackendABC
 
     def Transfer(self, Options: list[TransferOptions]):
-        if len(Options) == 0:
-            raise ValueError("No items in Options list")
+        Opt = Options[0]
+        # All the options should be the same. So we can just take the first one for the majority
 
-        if all(isinstance(Opts, TransferOptions) for Opts in Options):
-            Options = [cast(list[TransferOptions], Options)]
-        Options = cast(list[list[TransferOptions]], Options)
-        # We will always assume it is a list of pipetting steps in the following code.
+        MaxVolume = self._GetMaxTransferVolume(
+            Opt.SourceLiquidClassCategory, Opt.DestinationLiquidClassCategory
+        )
 
-        for ListedOptions in Options:
-            Opt = ListedOptions[0]
-            # All the options should be the same. So we can just take the first one for the majority
+        NumTransfers = ceil(Opt.TransferVolume / MaxVolume)
+        TransferVolume = Opt.TransferVolume / NumTransfers
+        # Find out how many transfers we need to do
 
-            MaxVolume = self.SupportedTips[-1].Tip.Volume
-            NumTransfers = ceil(Opt.TransferVolume / MaxVolume)
-            TransferVolume = Opt.TransferVolume / NumTransfers
-            # Find out how many transfers we need to do
+        PipetteTipInstance = self._GetTip(
+            Opt.SourceLiquidClassCategory,
+            Opt.DestinationLiquidClassCategory,
+            TransferVolume,
+        )
 
-            PipetteTipInstance = self._GetTip(
-                Opt.SourceLiquidClassCategory,
-                Opt.DestinationLiquidClassCategory,
-                TransferVolume,
-            )
+        PickupOptions = CORE96Head.Pickup.Options(
+            LabwareID=PipetteTipInstance.Tip.RackLabwareIDs[0]
+        )
+        # TODO. Need to use 1mL channels to move the tips to the support rack
 
-            PickupOptions = CORE96Head.Pickup.Options(
-                LabwareID=PipetteTipInstance.Tip.PickupSequence
-            )
-
-            AspirateOptions = CORE96Head.Aspirate.Options(
-                LabwareID=Opt.SourceLayoutItemInstance.LabwareID,
-                LiquidClass=str(
-                    self._GetLiquidClass(
-                        Opt.SourceLiquidClassCategory,
-                        TransferVolume,
-                    )
-                ),
-                Volume=TransferVolume,
-            )
-
-            DispenseOptions = CORE96Head.Dispense.Options(
-                Sequence=Options.SourceLayoutItemInstance.Sequence,
-                LiquidClass=str(
-                    self.GetLiquidClass(
-                        Options.SourceLiquidClassCategory, TransferVolume
-                    ).Name
-                ),
-                Volume=TransferVolume,
-            )
-
-            EjectOptions = CORE96Head.Eject.Options(
-                Sequence=PipetteTipInstance.WasteSequence
-            )
-
-            for _ in range(0, NumTransfers):
-                CORE96Head.Pickup.Command(
-                    CustomErrorHandling=self.CustomErrorHandling,
-                    Options=PickupOptions,
+        AspirateOptions = CORE96Head.Aspirate.Options(
+            LabwareID=Opt.SourceLayoutItemInstance.LabwareID,
+            LiquidClass=str(
+                self._GetLiquidClass(
+                    Opt.SourceLiquidClassCategory,
+                    TransferVolume,
                 )
+            ),
+            Volume=TransferVolume,
+        )
 
-                CORE96Head.Aspirate.Command(
-                    CustomErrorHandling=self.CustomErrorHandling,
-                    Options=AspirateOptions,
-                )
+        DispenseOptions = CORE96Head.Dispense.Options(
+            LabwareID=Opt.SourceLayoutItemInstance.LabwareID,
+            LiquidClass=str(
+                self._GetLiquidClass(Opt.SourceLiquidClassCategory, TransferVolume)
+            ),
+            Volume=TransferVolume,
+        )
 
-                CORE96Head.Dispense.Command(
-                    CustomErrorHandling=self.CustomErrorHandling,
-                    Options=DispenseOptions,
-                )
+        EjectOptions = CORE96Head.Eject.Options(
+            LabwareID=PipetteTipInstance.TipWasteLabwareID
+        )
 
-                CORE96Head.Eject.Command(
-                    CustomErrorHandling=self.CustomErrorHandling,
-                    Options=EjectOptions,
-                )
+        for _ in range(0, NumTransfers):
+            CORE96Head.Pickup.Command(
+                CustomErrorHandling=self.CustomErrorHandling,
+                Options=PickupOptions,
+            )
+
+            CORE96Head.Aspirate.Command(
+                CustomErrorHandling=self.CustomErrorHandling,
+                Options=AspirateOptions,
+            )
+
+            CORE96Head.Dispense.Command(
+                CustomErrorHandling=self.CustomErrorHandling,
+                Options=DispenseOptions,
+            )
+
+            CORE96Head.Eject.Command(
+                CustomErrorHandling=self.CustomErrorHandling,
+                Options=EjectOptions,
+            )
 
     def TimeToTransfer(self, Options: list[TransferOptions]) -> float:
         return 0
