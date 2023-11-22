a637906ff555abe2b4231dbab8000bbe6d3f1bdc
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-11-22 12:12:34 -0600

    Setup options for track gripper commands

--- PytomatedLiquidHandling/Driver/Hamilton/TrackGripper/GripPlate/Options.py
+++ PytomatedLiquidHandling/Driver/Hamilton/TrackGripper/GripPlate/Options.py
@@ -1,5 +1,42 @@
+from enum import Enum
+
+from pydantic import Field
+
 from ....Tools.AbstractClasses import OptionsABC
 
 
 class Options(OptionsABC):
-    ComPort: str
+    class YesNoOptions(Enum):
+        No = 0
+        Yes = 1
+
+    class GripModeOptions(Enum):
+        GripOnShortSide = 0
+        GripOnLongSide = 1
+
+    class MovementOptions(Enum):
+        Simple = 0
+        Complex = 1
+        Taught = 2
+
+    class LabwareOrientationOptions(Enum):
+        PositiveYAxis = 1
+        PositiveXAxis = 2
+        NegativeXAxis = 3
+
+    LabwareID: str
+    LabwareWidth: float
+    OpenWidth: float
+    GripHeight: float = 3
+    GripMode: GripModeOptions = GripModeOptions.GripOnShortSide
+    LabwareOrientation: LabwareOrientationOptions = (
+        LabwareOrientationOptions.PositiveYAxis
+    )
+    Movement: MovementOptions = MovementOptions.Simple
+    RetractDistance: float = 0
+    LiftupHeight: float = 0
+    TaughtPathName: str
+    CoordinatedMovement: bool = False
+    GripForcePercentage: int = Field(ge=0, le=100, default=100)
+    SpeedPercentage: int = Field(ge=0, le=100, default=50)
+    CollisionControl: YesNoOptions = YesNoOptions.Yes
