5b535fc81b4eca42564819aba9b942e37d957cec
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-11-29 14:08:03 -0600

    Bug fix. Good to go.

--- Examples/Driver/Stunner_Tray_OpenClose.py
+++ Examples/Driver/Stunner_Tray_OpenClose.py
@@ -1,15 +1,13 @@
 from PytomatedLiquidHandling.Driver.UnchainedLabs import (
     CloseTray,
     DefineExperiment,
-    StartMeasurement,
+    GetResults,
+    GetStatus,
     OpenTray,
-    GetResults,GetStatus,Reset
+    Reset,
+    StartMeasurement,
 )
 from PytomatedLiquidHandling.Driver.UnchainedLabs.Backend import StunnerBackend
-
-
-
-
 
 Backend = StunnerBackend(
     Identifier="Example Stunner",
@@ -26,11 +24,32 @@ Backend.ExecuteCommand(Command)
 Backend.WaitForResponseBlocking(Command)
 Backend.GetResponse(Command, CloseTray.Response)
 
-Command = DefineExperiment.Command(Options=DefineExperiment.ListedOptions(ExperimentName="Test",ApplicationName=DefineExperiment.ListedOptions.ApplicationNameOptions.ProteinSinglePoint))
-Command.Options.append(DefineExperiment.Options(SampleName="A",SamplePlateID="Plate 1",SamplePlatePosition="A1",SampleGroup=1))
-Command.Options.append(DefineExperiment.Options(SampleName="B",SamplePlateID="Plate 1",SamplePlatePosition="B1",SampleGroup=1))
-Command.Options.append(DefineExperiment.Options(SampleName="C",SamplePlateID="Plate 1",SamplePlatePosition="C1",SampleGroup=1))
-Command.Options.append(DefineExperiment.Options(SampleName="D",SamplePlateID="Plate 1",SamplePlatePosition="D1",SampleGroup=1))
+Command = DefineExperiment.Command(
+    Options=DefineExperiment.ListedOptions(
+        ExperimentName="Test",
+        ApplicationName=DefineExperiment.ListedOptions.ApplicationNameOptions.ProteinSinglePoint,
+    )
+)
+Command.Options.append(
+    DefineExperiment.Options(
+        SampleName="A", SamplePlateID="Plate 1", SamplePlatePosition="A1", SampleGroup=1
+    )
+)
+Command.Options.append(
+    DefineExperiment.Options(
+        SampleName="B", SamplePlateID="Plate 1", SamplePlatePosition="B1", SampleGroup=1
+    )
+)
+Command.Options.append(
+    DefineExperiment.Options(
+        SampleName="C", SamplePlateID="Plate 1", SamplePlatePosition="C1", SampleGroup=1
+    )
+)
+Command.Options.append(
+    DefineExperiment.Options(
+        SampleName="D", SamplePlateID="Plate 1", SamplePlatePosition="D1", SampleGroup=1
+    )
+)
 Backend.ExecuteCommand(Command)
 Backend.WaitForResponseBlocking(Command)
 Response1 = Backend.GetResponse(Command, DefineExperiment.Response)
@@ -41,18 +60,18 @@ Backend.ExecuteCommand(Command)
 Backend.WaitForResponseBlocking(Command)
 Response = Backend.GetResponse(Command, GetStatus.Response)
 
-Command = StartMeasurement.Command(Options=StartMeasurement.Options(PlateID=Response1.DefinedPlateIDs[0]))
+Command = StartMeasurement.Command(
+    Options=StartMeasurement.Options(PlateID=Response1.DefinedPlateIDs[0])
+)
 Backend.ExecuteCommand(Command)
 Backend.WaitForResponseBlocking(Command)
 Backend.GetResponse(Command, StartMeasurement.Response)
-
 
 
 import time
 
 try:
     while True:
-
         Command = GetStatus.Command()
         Backend.ExecuteCommand(Command)
         Backend.WaitForResponseBlocking(Command)
@@ -69,12 +88,16 @@ except:
     time.sleep(1)
 
 Command = GetResults.Command(Options=GetResults.ListedOptions())
-Command.Options += [GetResults.Options.SampleName,GetResults.Options.A280,GetResults.Options.PlatePosition]
+Command.Options += [
+    GetResults.Options.SampleName,
+    GetResults.Options.A280,
+    GetResults.Options.PlatePosition,
+]
 Backend.ExecuteCommand(Command)
 Backend.WaitForResponseBlocking(Command)
 Response = Backend.GetResponse(Command, GetResults.Response)
 
-print(Response.Results)
+print(Response.Results[0][GetResults.Options.SampleName])
 print(Response.ResultsPath)
 
 Backend.StopBackend()
--- PytomatedLiquidHandling/Driver/UnchainedLabs/GetStatus/Command.py
+++ PytomatedLiquidHandling/Driver/UnchainedLabs/GetStatus/Command.py
@@ -3,5 +3,5 @@ from ..Backend import UnchainedLabsCommandABC
 
 class Command(UnchainedLabsCommandABC):
     def _ExecuteCommandHelper(self, StunnerDLLObject) -> dict | Exception:
-        StatusCode, MeasurementInfo = StunnerDLLObject.Get_Status()
+        StatusCode, MeasurementInfo = StunnerDLLObject.Get_Status("")
         return dict(StatusCode=StatusCode, MeasurementInfo=MeasurementInfo)
