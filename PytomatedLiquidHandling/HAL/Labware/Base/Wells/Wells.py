f1791c58bd94b3bb2da1a7dc7a0ba73b9e38b536
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2023-12-14 14:03:34 -0600

    Testing integration of stunner plate.

--- PytomatedLiquidHandling/HAL/Labware/Base/Wells/Wells.py
+++ PytomatedLiquidHandling/HAL/Labware/Base/Wells/Wells.py
@@ -1,13 +1,13 @@
+from pydantic import dataclasses
+
 from .Layout import AlphaNumeric, Numeric
 from .Segment import Segment
-
-from pydantic import dataclasses
 
 
 @dataclasses.dataclass(kw_only=True)
 class Wells:
     Layout: AlphaNumeric | Numeric
-    SequencesPerWell: int
+    PositionsPerWell: int
     MaxVolume: float
     DeadVolume: float
     Segments: list[Segment]
