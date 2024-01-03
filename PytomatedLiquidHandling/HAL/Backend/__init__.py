f29887e23844cc425c0ba3ab853f5be036b9340d
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-01-03 10:30:13 -0600

    N/A

--- PytomatedLiquidHandling/HAL/Backend/__init__.py
+++ PytomatedLiquidHandling/HAL/Backend/__init__.py
@@ -12,11 +12,12 @@ HALDevice.HALDevices[MicrolabSTAR.__name__] = cast(Type[HALDevice], MicrolabSTAR
 HALDevice.HALDevices[VantageTrackGripperEntryExit.__name__] = cast(
     Type[HALDevice], VantageTrackGripperEntryExit
 )
+# Add microlab star and vantage to HALDevice so they can be loaded during configuration
 
 from PytomatedLiquidHandling.Driver.UnchainedLabs.Backend import StunnerBackend
 
 HALDevice.HALDevices[StunnerBackend.__name__] = cast(Type[HALDevice], StunnerBackend)
-
+# Add Stunner to HALDevice so they can be loaded during configuration
 
 Identifier = str
 Devices: dict[Identifier, Base.BackendABC] = dict()
