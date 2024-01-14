627029b24bbb7e879c442a1c1ce7fe5b9959f475
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-01-14 09:45:32 -0600

    ...

--- Tests/Driver/Hamilton/Microlab_STAR/FlipTubeTool.py
+++ Tests/Driver/Hamilton/Microlab_STAR/FlipTubeTool.py
@@ -1,7 +1,6 @@
 import os
 
 from loguru import logger
-
 from PytomatedLiquidHandling.Driver.Hamilton import FlipTubeTool
 from PytomatedLiquidHandling.Driver.Hamilton.Backend import MicrolabSTAR
 
@@ -9,19 +8,19 @@ from PytomatedLiquidHandling.Driver.Hamilton.Backend import MicrolabSTAR
 def Main():
     logger.info(f"Executing Main() from {__file__}")
 
-    logger.info(f"Initialize")
+    logger.info("Initialize")
     Command = FlipTubeTool.Initialize.Command(
         Options=FlipTubeTool.Initialize.Options(
-            ToolOrientation=FlipTubeTool.Initialize.Options.ToolOrientationOptions.Landscape
-        )
+            ToolOrientation=FlipTubeTool.Initialize.Options.ToolOrientationOptions.Landscape,
+        ),
     )
     Backend.ExecuteCommand(Command)
     Backend.WaitForResponseBlocking(Command)
     Backend.GetResponse(Command, FlipTubeTool.Initialize.Response)
 
-    logger.info(f"Pickup")
+    logger.info("Pickup")
     Command = FlipTubeTool.ToolsPickUp.Command(
-        Options=FlipTubeTool.ToolsPickUp.ListedOptions(LabwareID="FlipTubeTools")
+        Options=FlipTubeTool.ToolsPickUp.ListedOptions(LabwareID="FlipTubeTools"),
     )
     Command.Options.append(FlipTubeTool.ToolsPickUp.Options(ChannelNumber=1))
     Command.Options.append(FlipTubeTool.ToolsPickUp.Options(ChannelNumber=2))
@@ -31,61 +30,77 @@ def Main():
     Backend.WaitForResponseBlocking(Command)
     Backend.GetResponse(Command, FlipTubeTool.ToolsPickUp.Response)
 
-    logger.info(f"Open")
+    logger.info("Open")
     Command = FlipTubeTool.FlipTubeOpen.Command(Options=list())
     Command.Options.append(
         FlipTubeTool.FlipTubeOpen.Options(
-            LabwareID="FlipTubes", PositionID="1", ChannelNumber=1
-        )
+            LabwareID="FlipTubes",
+            PositionID="1",
+            ChannelNumber=1,
+        ),
     )
     Command.Options.append(
         FlipTubeTool.FlipTubeOpen.Options(
-            LabwareID="FlipTubes", PositionID="2", ChannelNumber=2
-        )
+            LabwareID="FlipTubes",
+            PositionID="2",
+            ChannelNumber=2,
+        ),
     )
     Command.Options.append(
         FlipTubeTool.FlipTubeOpen.Options(
-            LabwareID="Plate_1", PositionID="A1", ChannelNumber=3
-        )
+            LabwareID="Plate_1",
+            PositionID="A1",
+            ChannelNumber=3,
+        ),
     )
     Command.Options.append(
         FlipTubeTool.FlipTubeOpen.Options(
-            LabwareID="FlipTubes", PositionID="4", ChannelNumber=4
-        )
+            LabwareID="FlipTubes",
+            PositionID="4",
+            ChannelNumber=4,
+        ),
     )
     Backend.ExecuteCommand(Command)
     Backend.WaitForResponseBlocking(Command)
     Backend.GetResponse(Command, FlipTubeTool.FlipTubeOpen.Response)
 
-    logger.info(f"Close")
+    logger.info("Close")
     Command = FlipTubeTool.FlipTubeClose.Command(Options=list())
     Command.Options.append(
         FlipTubeTool.FlipTubeClose.Options(
-            LabwareID="FlipTubes", PositionID="1", ChannelNumber=1
-        )
+            LabwareID="FlipTubes",
+            PositionID="1",
+            ChannelNumber=1,
+        ),
     )
     Command.Options.append(
         FlipTubeTool.FlipTubeClose.Options(
-            LabwareID="FlipTubes", PositionID="2", ChannelNumber=2
-        )
+            LabwareID="FlipTubes",
+            PositionID="2",
+            ChannelNumber=2,
+        ),
     )
     Command.Options.append(
         FlipTubeTool.FlipTubeClose.Options(
-            LabwareID="FlipTubes", PositionID="3", ChannelNumber=3
-        )
+            LabwareID="FlipTubes",
+            PositionID="3",
+            ChannelNumber=3,
+        ),
     )
     Command.Options.append(
         FlipTubeTool.FlipTubeClose.Options(
-            LabwareID="FlipTubes", PositionID="4", ChannelNumber=4
-        )
+            LabwareID="FlipTubes",
+            PositionID="4",
+            ChannelNumber=4,
+        ),
     )
     Backend.ExecuteCommand(Command)
     Backend.WaitForResponseBlocking(Command)
     Backend.GetResponse(Command, FlipTubeTool.FlipTubeOpen.Response)
 
-    logger.info(f"Eject")
+    logger.info("Eject")
     Command = FlipTubeTool.ToolsEject.Command(
-        Options=FlipTubeTool.ToolsEject.Options(LabwareID="FlipTubeTools")
+        Options=FlipTubeTool.ToolsEject.Options(LabwareID="FlipTubeTools"),
     )
     Backend.ExecuteCommand(Command)
     Backend.WaitForResponseBlocking(Command)
