3460f6de9dd4653db3922b72f28eed5a1d3e0265
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-01-25 13:14:50 -0600

    ...

--- tests/hal/HamiltonVantageEE_TG_Stunner_Centrifuge/hamilton_pipette_test.py
+++ tests/hal/HamiltonVantageEE_TG_Stunner_Centrifuge/hamilton_pipette_test.py
@@ -43,95 +43,41 @@ pipette._pickup(
 
 input("enter")
 
+position_ids = ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1"]
+well_volumes = [5, 25, 50, 100, 150, 200, 300, 400]
+volumes = [5, 25, 50, 100, 150, 200, 300, 400]
+mix_cycles = [0, 0, 0, 5, 0, 0, 0, 5]
+mix_volumes = [0, 0, 0, 100, 0, 0, 0, 400]
+
+
 pipette._aspirate(
     [
         hal.pipette._AspirateDispenseOptions(
             ChannelNumber=1,
             LayoutItem=plate,
-            PositionID="A1",
-            WellVolume=10,
-            MixCycles=0,
-            MixVolume=0,
+            PositionID=position_ids[i],
+            WellVolume=well_volumes[i],
+            MixCycles=mix_cycles[i],
+            MixVolume=mix_volumes[i],
             LiquidClass="HighVolume_Water_DispenseSurface_Empty",
-            Volume=30,
-        ),
-        hal.pipette._AspirateDispenseOptions(
-            ChannelNumber=2,
-            LayoutItem=plate,
-            PositionID="B1",
-            WellVolume=50,
-            MixCycles=0,
-            MixVolume=0,
-            LiquidClass="HighVolume_Water_DispenseSurface_Empty",
-            Volume=30,
-        ),
-        hal.pipette._AspirateDispenseOptions(
-            ChannelNumber=3,
-            LayoutItem=plate,
-            PositionID="C1",
-            WellVolume=100,
-            MixCycles=0,
-            MixVolume=0,
-            LiquidClass="HighVolume_Water_DispenseSurface_Empty",
-            Volume=30,
-        ),
-        hal.pipette._AspirateDispenseOptions(
-            ChannelNumber=4,
-            LayoutItem=plate,
-            PositionID="D1",
-            WellVolume=150,
-            MixCycles=5,
-            MixVolume=100,
-            LiquidClass="HighVolume_Water_DispenseSurface_Empty",
-            Volume=30,
-        ),
+            Volume=volumes[i],
+        )
+        for i in range(8)
     ],
 )
+
+input("enter")
 
 input("enter")
 
 pipette._eject(
     [
         hal.pipette._EjectOptions(
-            ChannelNumber=1,
+            ChannelNumber=i + 1,
             LabwareID="VStarWaste_16Pos_0001",
-            PositionID="1",
-        ),
-        hal.pipette._EjectOptions(
-            ChannelNumber=2,
-            LabwareID="VStarWaste_16Pos_0001",
-            PositionID="2",
-        ),
-        hal.pipette._EjectOptions(
-            ChannelNumber=3,
-            LabwareID="VStarWaste_16Pos_0001",
-            PositionID="3",
-        ),
-        hal.pipette._EjectOptions(
-            ChannelNumber=4,
-            LabwareID="VStarWaste_16Pos_0001",
-            PositionID="4",
-        ),
-        hal.pipette._EjectOptions(
-            ChannelNumber=5,
-            LabwareID="VStarWaste_16Pos_0001",
-            PositionID="5",
-        ),
-        hal.pipette._EjectOptions(
-            ChannelNumber=6,
-            LabwareID="VStarWaste_16Pos_0001",
-            PositionID="6",
-        ),
-        hal.pipette._EjectOptions(
-            ChannelNumber=7,
-            LabwareID="VStarWaste_16Pos_0001",
-            PositionID="7",
-        ),
-        hal.pipette._EjectOptions(
-            ChannelNumber=8,
-            LabwareID="VStarWaste_16Pos_0001",
-            PositionID="8",
-        ),
+            PositionID=str(i + 1),
+        )
+        for i in range(8)
     ],
 )
 
