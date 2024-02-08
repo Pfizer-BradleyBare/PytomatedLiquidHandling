1c4c06dc16ce711859861e21fb402d3e73f35d16
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-02-08 15:03:53 -0600

    ...

--- plh/hal/tip/exceptions.py
+++ plh/hal/tip/exceptions.py
@@ -1,17 +1,25 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import Any
 
-from plh.hal.exceptions import HALError
+from plh.hal.exceptions import UserInputRequiredError
 
-from .tip_base import TipBase
+from .tip_base import AvailablePosition, TipBase
 
 
 @dataclass
-class TierOutOfTipsError(HALError):
+class TierOutOfTipsError(UserInputRequiredError):
     """The Tip device has no more tips available in the teir. A tip discard / reload event is required."""
 
     error_device: TipBase
 
     def __str__(self) -> str:
         return self.error_device.identifier
+
+    def kwargs(self: TierOutOfTipsError) -> dict[str, type]:
+        return {"available_positions": AvailablePosition}
+
+    def callback(self: TierOutOfTipsError, **kwargs: dict[str, Any]) -> None:
+        # TODO
+        ...
