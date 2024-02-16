4597cf06110cc89213abc2f928b7053f3cbea850
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-02-15 19:45:24 -0600

    ...

--- plh/api/deck/container/container.py
+++ plh/api/deck/container/container.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from dataclasses import InitVar, dataclass, field
 from enum import Enum, auto
 from typing import Callable, ClassVar, Generic, TypeVar, cast
 
@@ -134,6 +134,12 @@ class Liquid:
     ```Volatility```, ```Viscosity```, ```Homogeneity```,and ```Polarity```.
     """
 
+    def __hash__(self:Liquid) -> int:
+        return hash(self.name)
+
+    def __eq__(self:Liquid, __value: Liquid) -> bool:
+        return self.name == __value.name
+
     name: str
     """The liquid name."""
 
@@ -159,7 +165,9 @@ class Liquid:
 class Well:
     """A physical well that contains a liquid or mixture of liquids."""
 
-    liquid_volumes: dict[str, LiquidVolume] = field(
+    initial_liquids: InitVar[list[LiquidVolume]] = field(default=[])
+
+    liquids: dict[Liquid, float] = field(
         init=False,
         default_factory=dict,
     )
@@ -169,9 +177,15 @@ class Well:
     _hashable_value: int = field(init=False)
     """Simple way to define a custom hashable value for this class to make it a dict key."""
 
-    def __post_init__(self: Well) -> None:
+    def __post_init__(self: Well, initial_liquids: list[LiquidVolume]) -> None:
         self._hashable_value = Well._hashable_counter
         Well._hashable_counter += 1
+
+        for liquid_volume in initial_liquids:
+            if liquid_volume.liquid in self.liquids:
+                self.liquids[liquid_volume.liquid] += liquid_volume.volume
+            else:
+                self.liquids[liquid_volume.liquid] = liquid_volume.volume
 
     def __hash__(self: Well) -> int:
         return hash(self._hashable_value)
@@ -183,9 +197,7 @@ class Well:
 
     def get_total_volume(self: Well) -> float:
         """Total volume present in the well."""
-        return sum(
-            [liquid_volume.volume for liquid_volume in self.liquid_volumes.values()],
-        )
+        return sum(self.liquids.values())
 
     def aspirate(self: Well, volume: float) -> list[LiquidVolume]:
         """Aspirate a volume from the well. Returns a list of (liquid,volume) that was aspirated."""
@@ -199,16 +211,16 @@ class Well:
 
         removed_fraction = volume / total_volume
 
-        for key, liquid_volume in self.liquid_volumes.items():
-            removed_volume = liquid_volume.volume * removed_fraction
-            new_volume = liquid_volume.volume - removed_volume
+        for liquid, volume in self.liquids.items():
+            removed_volume = volume * removed_fraction
+            new_volume = volume - removed_volume
 
-            aspirated_liquids.append(LiquidVolume(liquid_volume.liquid, removed_volume))
+            aspirated_liquids.append(LiquidVolume(liquid, removed_volume))
 
             if new_volume > 0:
-                self.liquid_volumes[key] = LiquidVolume(liquid_volume.liquid, new_volume)
+                self.liquids[liquid] = new_volume
             else:
-                del self.liquid_volumes[key]
+                del self.liquids[liquid]
 
         return aspirated_liquids
 
@@ -216,29 +228,27 @@ class Well:
         """Dispense a list of (liquid,volume) into a well."""
         for dispensed_liquid_volume in liquid_volumes:
 
-            name = dispensed_liquid_volume.liquid.name
+            liquid = dispensed_liquid_volume.liquid
 
             current_volume = 0
-            if name in self.liquid_volumes:
-                current_volume = self.liquid_volumes[name].volume
+            if liquid in self.liquids:
+                current_volume = self.liquids[liquid]
 
-            self.liquid_volumes[name] = LiquidVolume(
-                dispensed_liquid_volume.liquid,
-                dispensed_liquid_volume.volume + current_volume,
-            )
+            self.liquids[liquid] = dispensed_liquid_volume.volume + current_volume
+
 
     def get_well_property(
         self: Well,
         property_function: Callable[[Liquid], PropertyWeight[T]],
     ) -> T:
         """Get a specific liquid property based on the composition of liquids in the well."""
-        if len(self.liquid_volumes) == 0:
+        if len(self.liquids) == 0:
             msg = "Well is empty."
             raise ValueError(msg)
 
         property_volumes = [
-            PropertyWeightVolume(property_function(liquid_volume.liquid), liquid_volume.volume)
-            for liquid_volume in self.liquid_volumes.values()
+            PropertyWeightVolume(property_function(liquid), volume)
+            for liquid,volume in self.liquids.items()
         ]
 
         return cast(T,cast(LiquidPropertyBase,property_volumes[0].property_weight.property).calculate_composition_property(
