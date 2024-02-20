61a21a98e9734fa85575497f7d15147de9a53712
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-02-20 11:11:49 -0600

    remove UI meta data.

--- plh/hal/labware/labware_base.py
+++ plh/hal/labware/labware_base.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from pydantic import FilePath, dataclasses
+from pydantic import dataclasses
 
 from plh.hal.tools import HALDevice
 
@@ -12,27 +12,6 @@ from .transport_offsets import TransportOffsets
 @dataclasses.dataclass(kw_only=True, eq=False)
 class LabwareBase(HALDevice):
     """Type of physical labware (200uL plate, lid, etc.)."""
-
-    part_number: str
-    """Part number of the labware."""
-
-    image_path_2d: FilePath | None
-    """Full path to a 2D image."""
-
-    image_path_3d: FilePath | None
-    """Full path to a 3D image."""
-
-    model_path_3d: FilePath | None
-    """Full path to a 3D model."""
-
-    container_image_path_2d: FilePath | None
-    """Full path to a 2D image."""
-
-    container_image_path_3d: FilePath | None
-    """Full path to a 3D image."""
-
-    container_model_path_3d: FilePath | None
-    """Full path to a 3D model."""
 
     dimensions: Dimensions
     """Dimensions object."""
