0cb36b1919357fb02b8a3a67fd525367bd3ca9c2
Bradley Bare <Bradley.Bare@pfizer.com>
2024-03-04 09:27:03 -0600

    ...

--- tests/driver/hamilton/VantageTrackGripperEntryExit/PlateEditor96.py
+++ tests/driver/hamilton/VantageTrackGripperEntryExit/PlateEditor96.py
@@ -3,10 +3,10 @@ import pathlib
 from loguru import logger
 
 from plh.driver.HAMILTON import PlateEditor96
-from plh.driver.HAMILTON.backend import MicrolabSTAR
+from plh.driver.HAMILTON.backend import VantageTrackGripperEntryExit
 
 
-def main(backend: MicrolabSTAR) -> None:
+def main(backend: VantageTrackGripperEntryExit) -> None:
     logger.info(f"Executing main() from {__file__}")
 
     logger.info("EditPlate96")
@@ -24,7 +24,7 @@ def main(backend: MicrolabSTAR) -> None:
 if __name__ == "__main__":
     logger.enable("plh")
 
-    backend = MicrolabSTAR(
+    backend = VantageTrackGripperEntryExit(
         identifier="Example Star",
         simulation_on=True,
         deck_layout=pathlib.Path(__file__).parent / "SimulationLayout.lay",
