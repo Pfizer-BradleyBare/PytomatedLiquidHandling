6bf6cbff1b38621ad455c11083017f37c9b6b7d7
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-04-23 11:08:42 -0500

    Add api init and deinit.

--- tests/hal/HamiltonVantageEE_TG_Stunner_Centrifuge/incubate_api.py
+++ tests/hal/HamiltonVantageEE_TG_Stunner_Centrifuge/incubate_api.py
@@ -5,13 +5,15 @@ import os
 from loguru import logger
 
 from plh import hal
-from plh.api import incubate
+from plh.api import deinitialize, incubate, initialize
 from plh.api.container import Well
 from plh.api.tools import loaded_labware
 
 logger.enable("plh")
 
 hal.load_yaml_configuration(os.path.join(os.path.dirname(__file__), "Config"))
+
+initialize()
 
 biorad = hal.layout_item.devices["Carrier42_Pos1_Biorad200uLPCRPlate"]
 
@@ -25,3 +27,5 @@ incubate.assert_compatible_device(50, 0, biorad.labware)
 reservation = incubate.reserve(50, 0, well)[0]
 incubate.start(reservation)
 incubate.release(reservation)
+
+deinitialize()
