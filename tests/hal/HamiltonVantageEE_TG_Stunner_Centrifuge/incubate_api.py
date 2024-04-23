c556c5ad2d1e95678a96782b6039079bc1344bfb
Bradley <Bradley.Bare@pfizer.com>
2024-04-23 13:54:19 -0500

    ...

--- tests/hal/HamiltonVantageEE_TG_Stunner_Centrifuge/incubate_api.py
+++ tests/hal/HamiltonVantageEE_TG_Stunner_Centrifuge/incubate_api.py
@@ -8,7 +8,7 @@ from plh import hal
 from plh.api import deinitialize, incubate, initialize
 from plh.api.container import Well
 from plh.api.tools import loaded_labware
-
+from plh.api.deck_manager import move_container
 logger.enable("plh")
 
 hal.load_yaml_configuration(os.path.join(os.path.dirname(__file__), "Config"))
@@ -26,6 +26,10 @@ ll.create_assignments(well, 1)
 incubate.assert_compatible_device(50, 0, biorad.labware)
 reservation = incubate.reserve(50, 0, well)[0]
 incubate.start(reservation)
+import time
+time.sleep(10)
 incubate.release(reservation)
+
+move_container([well],[biorad.deck_location])
 
 deinitialize()
