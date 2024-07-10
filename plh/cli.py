39f41b9dc76de25b3975f87b405d52c6661669e4
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-07-10 13:13:28 -0500

    Partial update of cli for new folder structure

--- plh/cli.py
+++ plh/cli.py
@@ -14,7 +14,7 @@ def plh() -> None:
 
 
 @plh.command()
-def install_venus_dependencies() -> None:
+def install_venus() -> None:
     """Installs all Venus libraries required by plh by invoking a set of Hamilton supplied .exe files."""
     if not pathlib.Path("C:\\Program Files (x86)\\HAMILTON").exists():
         print(
@@ -36,10 +36,29 @@ def install_venus_dependencies() -> None:
     backend_library_folder = backend_path / "library"
     backend_method_folder = backend_path / "method"
 
-    hamilton_bin_folder = hamilton_path / "Bin"
-    hamilton_labware_folder = hamilton_path / "LabWare"
-    hamilton_library_folder = hamilton_path / "Library"
-    hamilton_methods_folder = hamilton_path / "Methods"
+    hamilton_bin_folder = hamilton_path / "Bin" / "plh"
+    hamilton_labware_folder = hamilton_path / "LabWare" / "plh"
+    hamilton_library_folder = hamilton_path / "Library" / "plh"
+    hamilton_methods_folder = hamilton_path / "Methods" / "plh"
+
+    hamilton_bin_folder.unlink(missing_ok=True)
+    hamilton_labware_folder.unlink(missing_ok=True)
+    hamilton_library_folder.unlink(missing_ok=True)
+    hamilton_methods_folder.unlink(missing_ok=True)
+    # Clean the folders first
+
+    hamilton_bin_folder.mkdir()
+    hamilton_labware_folder.mkdir()
+    hamilton_library_folder.mkdir()
+    hamilton_methods_folder.mkdir()
+    # Make the folders
+
+    shutil.copytree(backend_installer_folder, hamilton_bin_folder)
+    shutil.copytree(backend_labware_folder, hamilton_labware_folder)
+    shutil.copytree(backend_library_folder, hamilton_library_folder)
+    shutil.copytree(backend_method_folder, hamilton_methods_folder)
+
+    return
 
     bin_folder = pathlib.Path(hamilton_bin.__file__).parent
 
