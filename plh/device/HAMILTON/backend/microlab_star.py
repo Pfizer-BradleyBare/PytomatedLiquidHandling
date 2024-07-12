d1c1763319ccda70f3a207a290761a9f5ec97123
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-07-12 13:01:11 -0500

    Update paths

--- plh/device/HAMILTON/backend/microlab_star.py
+++ plh/device/HAMILTON/backend/microlab_star.py
@@ -11,9 +11,9 @@ class MicrolabSTAR(HamiltonBackendBase):
     method: pathlib.Path = field(
         init=False,
         default=pathlib.Path(
-            "C:\\Program Files (x86)\\HAMILTON\\Library\\plh\\plh\\driver\\HAMILTON\\backend\\library\\star\\method\\BasicMethod.med",
+            "C:\\Program Files (x86)\\HAMILTON\\Methods\\plh\\star\\BasicMethod.med",
         ),
     )
     deck_layout: pathlib.Path = pathlib.Path(
-        "C:\\Program Files (x86)\\HAMILTON\\Library\\plh\\plh\\driver\\HAMILTON\\backend\\library\\star\\Method\\ExampleLayout.lay",
+        "C:\\Program Files (x86)\\HAMILTON\\Methods\\plh\\star\\ExampleLayout.lay",
     )
--- plh/device/HAMILTON/backend/vantage_track_gripper_entry_exit.py
+++ plh/device/HAMILTON/backend/vantage_track_gripper_entry_exit.py
@@ -11,9 +11,9 @@ class VantageTrackGripperEntryExit(HamiltonBackendBase):
     method: pathlib.Path = field(
         init=False,
         default=pathlib.Path(
-            "C:\\Program Files (x86)\\HAMILTON\\Library\\plh\\plh\\driver\\HAMILTON\\backend\\library\\vantage\\Method\\BasicMethod.med",
+            "C:\\Program Files (x86)\\HAMILTON\\Methods\\plh\\vantage\\BasicMethod.med",
         ),
     )
     deck_layout: pathlib.Path = pathlib.Path(
-        "C:\\Program Files (x86)\\HAMILTON\\Library\\plh\\plh\\driver\\HAMILTON\\backend\\library\\vantage\\Method\\ExampleLayout.lay",
+        "C:\\Program Files (x86)\\HAMILTON\\Methods\\plh\\vantage\\ExampleLayout.lay",
     )
